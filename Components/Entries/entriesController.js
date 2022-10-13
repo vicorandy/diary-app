128456a4cd59858cc935fe55c8258975fa19d3ce
vicorandy <vicorandyrichard26@gmail.com>
2022-10-13 12:41:22 +0100

    adding authorization to entries route

--- Components/Entries/entriesController.js
+++ Components/Entries/entriesController.js
@@ -4,89 +4,115 @@ const Entries = require('./entriesModel');
 
 // FECTHING ALL ENTRIES
 async function getAllEntries(req, res) {
-  const { userid } = req.user;
-  const entries = await Entries.findAll({ where: { userid } });
-  const count = entries.length;
-  res
-    .status(StatusCodes.OK)
-    .json({ message: 'All entries', data: entries, count });
+  try {
+    const { userid } = req.user;
+    const entries = await Entries.findAll({ where: { userid } });
+    const count = entries.length;
+    res
+      .status(StatusCodes.OK)
+      .json({ message: 'All entries', data: entries, count });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ message: 'Something went wrong' });
+  }
 }
 
 // FETCHING A SINGLE ENTRY
 async function getSingleEntry(req, res) {
-  const { userid } = req.user;
-  const { id } = req.params;
-  const entry = await Entries.findOne({ where: { id, userid } });
-  const count = entry.length;
-  if (!entry) {
-    res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ message: `no entry with the id: ${id}` });
-    throw new InvalidIdError(`no entry with the id: ${id}`);
-  }
-  if (entry) {
-    res
-      .status(StatusCodes.OK)
-      .json({ message: 'Request successful', data: entry, count });
+  try {
+    const { userid } = req.user;
+    const { id } = req.params;
+    const entry = await Entries.findOne({ where: { id, userid } });
+    if (!entry) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: `no entry with the id: ${id}` });
+      throw new InvalidIdError(`no entry with the id: ${id}`);
+    }
+    const count = entry.length;
+    if (entry) {
+      res
+        .status(StatusCodes.OK)
+        .json({ message: 'Request successful', data: entry, count });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ message: 'Something went wrong' });
   }
 }
 
 // CREATING A NEW ENTRY
 async function createEntry(req, res) {
-  const { userid } = req.user;
-  const { title, entry } = req.body;
-  const data = await Entries.create({
-    title,
-    entry,
-    userid,
-  });
-  res
-    .status(StatusCodes.CREATED)
-    .json({ message: 'entry has been logged ', data });
+  try {
+    const { userid } = req.user;
+    const { title, entry } = req.body;
+    const data = await Entries.create({
+      title,
+      entry,
+      userid,
+    });
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: 'entry has been logged ', data });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ messsage: 'Something went wrong' });
+  }
 }
 
 // EDITING A SINGLE ENRTY
 async function editEntry(req, res) {
-  const { userid } = req.user;
+  try {
+    const { userid } = req.user;
 
-  const { id } = req.params;
-  const { entry, title } = req.body;
+    const { id } = req.params;
+    const { entry, title } = req.body;
 
-  if (!entry || !title) {
-    res.status(StatusCodes.BAD_REQUEST);
-    res.json({ message: 'please enter all fields' });
-  }
-  const data = await Entries.findOne({ where: { id, userid } });
-  if (!data) {
-    res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ message: `no entry with the id: ${id}` });
-    throw new InvalidIdError(`no entry with the id: ${id}`);
-  }
-  if (data) {
-    await Entries.update({ entry, title }, { where: { id, userid } });
-    res.status(StatusCodes.OK).json({ message: 'entry has been edited' });
+    if (!entry || !title) {
+      res.status(StatusCodes.BAD_REQUEST);
+      res.json({ message: 'please enter all fields' });
+    }
+    const data = await Entries.findOne({ where: { id, userid } });
+    if (!data) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: `no entry with the id: ${id}` });
+      throw new InvalidIdError(`no entry with the id: ${id}`);
+    }
+    if (data) {
+      await Entries.update({ entry, title }, { where: { id, userid } });
+      res.status(StatusCodes.OK).json({ message: 'entry has been edited' });
+    }
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ message: 'something went wrong' });
   }
 }
 
 // DELETING A SINGLE ENTRY
 async function deleteEntry(req, res) {
-  const { userid } = req.user;
-  const { id } = req.params;
-  const data = await Entries.findOne({ where: { id, userid } });
+  try {
+    const { userid } = req.user;
+    const { id } = req.params;
+    const data = await Entries.findOne({ where: { id, userid } });
 
-  if (!data) {
-    res.status(StatusCodes.NOT_FOUND);
-    res.json({ message: `no entry with the id: ${id}` });
-    throw new InvalidIdError(`no entry with the id: id}`);
-  }
+    if (!data) {
+      res.status(StatusCodes.NOT_FOUND);
+      res.json({ message: `no entry with the id: ${id}` });
+      throw new InvalidIdError(`no entry with the id: id}`);
+    }
 
-  if (data) {
-    await Entries.destroy({ where: { id, userid } });
-    res.status(StatusCodes.OK);
-    res.json({
-      message: `the entry with the id: ${id} has been deleted sucessfully`,
-    });
+    if (data) {
+      await Entries.destroy({ where: { id, userid } });
+      res.status(StatusCodes.OK);
+      res.json({
+        message: `the entry with the id: ${id} has been deleted sucessfully`,
+      });
+    }
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.json({ message: 'Something went wrong' });
   }
 }
 
