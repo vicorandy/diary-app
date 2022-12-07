0917bad7ee5644e74afa6c3971c7b6d13480e547
vicorandy <vicorandyrichard26@gmail.com>
2022-12-07 13:56:50 +0100

    fixing bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,5 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-    'no-param-reassign': 'off',
-    'prefer-regex-literals': 'off',
-    'no-undef': 'off',
-    'no-console:': 'off',
-  },
+  rules: {},
 };
--- testSuite.js
+++
@@ -1,9 +0,0 @@
-const utilsTest = require('./Utils/test');
-const usersTest = require('./Components/Users/usersTest');
-const entriesTest = require('./Components/Entries/entriesTest');
-const authenticationTest = require('./MiddelWare/authTest');
-
-usersTest();
-utilsTest();
-entriesTest();
-authenticationTest();
