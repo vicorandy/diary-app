9d906f77e4fb87cae1827da16ffe1065be2f4951
vicorandy <vicorandyrichard26@gmail.com>
2022-12-07 12:27:46 +0100

    adding more test

--- MiddelWare/auth.js
+++ MiddelWare/auth.js
@@ -8,7 +8,7 @@ async function authorization(req, res, next) {
     if (!authHeader || !authHeader.startsWith('Bearer')) {
       res.status(401);
       res.json({ message: 'invalid user token' });
-      throw new Error('invalid user id');
+      return;
     }
     const token = authHeader.split(' ')[1];
     //  verifying user token
@@ -21,10 +21,10 @@ async function authorization(req, res, next) {
     next();
   } catch (error) {
     if (error.name === 'JsonWebTokenError') {
-      res.status(StatusCodes.UNAUTHORIZED);
+      res.status(401);
       res.json({ message: 'invalid user token' });
     } else {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+      res.status(500);
       res.json({ message: 'Something went wrong' });
     }
   }
--- testSuite.js
+++ testSuite.js
@@ -1,7 +1,9 @@
 const utilsTest = require('./Utils/test');
 const usersTest = require('./Components/Users/usersTest');
 const entriesTest = require('./Components/Entries/entriesTest');
+const authenticationTest = require('./MiddelWare/authTest');
 
 usersTest();
 utilsTest();
 entriesTest();
+authenticationTest();
