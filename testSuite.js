f066522f2253c8b8f2391024c5f7e50f65f6e578
vicorandy <vicorandyrichard26@gmail.com>
2022-12-08 16:40:01 +0100

    third test with travis

--- testSuite.js
+++ testSuite.js
@@ -1,7 +1,5 @@
-const utilsTest = require('./Utils/test');
 const usersTest = require('./Components/Users/usersTest');
 const entriesTest = require('./Components/Entries/entriesTest');
 
 usersTest();
-utilsTest();
 entriesTest();
